b1b94042806b730594a7341821b8642c0b549bae
KARTHIKEYA RAVI <karthikeya.raavi@gmail.com>
2019-03-18 19:50:33 -0400

    automation code

---
+++ WebPageTest.java
@@ -0,0 +1,66 @@
+package seleniumproject.com.testwebpage;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebPageTest {
+	
+	public static void main(String[]args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\KARTHIKEYA\\Desktop\\selenium\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://springpetclinic-env.kfqhm5ccsp.us-east-1.elasticbeanstalk.com/");
+		
+		
+		
+		String expectedTitle = "http://springpetclinic-env.kfqhm5ccsp.us-east-1.elasticbeanstalk.com/";
+		   
+		   //Actual Title
+		   String actualTitle = driver.getTitle();
+		   
+		   //Conditions
+		   if(actualTitle.equals(expectedTitle)){
+		   
+		   System.out.println("Title Matched");
+		   
+		   }else 
+		   {
+		   
+		   System.out.println("Title didn't match");
+		   }
+		   
+		   driver.manage().window().maximize();
+		   
+		driver.findElement(By.xpath("//*[@id=\"main-navbar\"]/ul/li[2]/a")).click(); //Home page
+		System.out.println("click");
+		driver.findElement(By.xpath("//*[@id=\"main-navbar\"]/ul/li[3]/a/span[2]")).click(); //main bar
+		Thread.sleep(2000);
+		//driver.findElement(By.id("lastName")).sendKeys("anusha"); //lastname
+		//driver.findElement(By.xpath("//*[@id=\"search-owner-form\"]/div[2]/div/button")).click(); // find owner
+		driver.findElement(By.xpath("/html/body/div/div/a")).click();  //add owner
+		driver.findElement(By.id("firstName")).sendKeys("anoosha");
+		driver.findElement(By.id("lastName")).sendKeys("Jagarlamudi");
+		driver.findElement(By.id("address")).sendKeys("20816");
+		driver.findElement(By.id("city")).sendKeys("sterling");
+		driver.findElement(By.id("telephone")).sendKeys("56780988");
+		driver.findElement(By.xpath("//*[@id=\"add-owner-form\"]/div[2]/div/button")).submit();
+		driver.findElement(By.xpath("/html/body/div/div/a[1]")).click();
+		 driver.findElement(By.id("telephone")).sendKeys("5678098812");
+		  
+		 
+		 
+		driver.findElement(By.xpath("//*[@id=\"add-owner-form\"]/div[2]/div/button")).click();
+		
+		driver.findElement(By.xpath("//*[@id=\"main-navbar\"]/ul/li[4]/a/span[2]")).click();
+		//*[@id="main-navbar"]/ul/li[4]/a/span[2]
+		
+		driver.findElement(By.linkText("View as XML")).click();
+		
+		driver.close();
+		
+	
+		
+	}
+
+}
