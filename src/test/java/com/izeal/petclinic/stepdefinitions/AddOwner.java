8c987efd7f1cfec6c5409cc8b85e3892d77423f6
lnkaruturi <lnkaruturi@gmail.com>
2019-03-19 20:44:27 -0400

    Taking Screen shot

--- src/test/java/com/izeal/petclinic/stepdefinitions/AddOwner.java
+++ src/test/java/com/izeal/petclinic/stepdefinitions/AddOwner.java
@@ -1,13 +1,17 @@
 package com.izeal.petclinic.stepdefinitions;
 
-import java.net.MalformedURLException;
+import java.io.File;
 import java.net.URL;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.Platform;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.google.common.io.Files;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -15,7 +19,7 @@ import cucumber.api.java.en.Then;
 public class AddOwner {
 	WebDriver w;
 	String Node;
-	
+
 	@Given("^Open Home Page forElasticbeanstalk$")
 	public void open_Home_Page_forElasticbeanstalk() throws Throwable {
 		Node = "http://192.168.1.221:4444/wd/hub";
@@ -34,29 +38,31 @@ public class AddOwner {
 	public void click_on_Find_owners_and_click_on_Add_owner() throws Throwable {
 		w.findElement(By.partialLinkText("OWNERS")).click();
 		w.findElement(By.xpath("/html/body/div/div/a")).click(); // add owner
-		
 
-		
 	}
 
 	@Given("^Enter  these values \"([^\"]*)\",\"([^\"]*)\",\"([^\"]*)\",\"([^\"]*)\",\"([^\"]*)\"$")
-	public void enter_these_values(String firstname, String lastname, String address, String city, String telephone) throws Throwable {
-		
+	public void enter_these_values(String firstname, String lastname, String address, String city, String telephone)
+			throws Throwable {
+
 		w.findElement(By.id("firstName")).sendKeys(firstname);
 		w.findElement(By.id("lastName")).sendKeys(lastname);
 		w.findElement(By.id("address")).sendKeys(address);
 		w.findElement(By.id("city")).sendKeys(city);
 		w.findElement(By.id("telephone")).sendKeys(telephone);
-		
-		
+
 	}
 
 	@Then("^Click On add owner$")
 	public void click_On_add_owner() throws Throwable {
 		w.findElement(By.xpath("//*[@id=\"add-owner-form\"]/div[2]/div/button")).submit();
 		w.findElement(By.xpath("/html/body/div/div/a[1]")).click();
-		w.close();
+		File src = ((TakesScreenshot) w).getScreenshotAs(OutputType.FILE);
+		Files.copy(src, new File("C:\\Users\\LAKSHMI NARAYANA\\git\\petclinic-test\\target\\1.png"));
+		w.quit();
 
 	}
+
+	
 
 }
